000ba6a3801c22d602fa55a473ad83c8b20a8665
Travis Nesland <tnesland@gmail.com>
2018-11-11 00:39:44 -0500

    Upgrade fisher and remove unused plugins

--- fish/config.fish
+++ fish/config.fish
@@ -17,7 +17,6 @@ alias tmux="tmux -2"
 export EDITOR="vim"
 export LANG="en_US.UTF-8"
 export LC_ALL="en_US.UTF-8"
-export NVM_DIR="$HOME/.nvm"
 export PAGER="less"
 export PATH="$HOME/bin:/usr/local/bin:/usr/local/sbin:"(brew --prefix qt@5.5)"/bin:$PATH"
 export TERM=xterm-256color
