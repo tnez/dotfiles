fcc446759f18517321b9df0323bf729d8ba86d55
Travis Nesland <tnesland@gmail.com>
2018-12-11 15:51:46 -0500

    Autocomplete t function with project directories
    When using a `--project` flag, the command will now autocomplete with
    the directory listing from `$HOME/Projects`.

--- fish/tnez/functions/t.fish
+++ fish/tnez/functions/t.fish
@@ -1,4 +1,17 @@
 function t -d "Attach to a tmux session based on dirname"
+  if test "$argv[1]" = "--project"
+    set bookmarked_dir (pwd)
+
+    cd "$HOME/Projects/$argv[2]"
+    _open_or_attach_to_session "$argv[2]"
+
+    cd "$bookmarked_dir"
+  else
+    _open_or_attach_to_session "$argv[1]"
+  end
+end
+
+function _open_or_attach_to_session
   if [ -z "$argv" ]
     set session_name (basename (PWD) | tr . -)
   else
@@ -18,4 +31,5 @@ function t -d "Attach to a tmux session based on dirname"
   tmux switch-client -t "$session_name"
 end
 
-complete -f -c t -a "(tmux list-sessions -F '#{session_name}')" -f
+complete -x -c t -d 'Connect to existing session' -a "(tmux list-sessions -F '#{session_name}')"
+complete -x -c t -l project -d 'Start/Connect to project session' -a "(ls $HOME/Projects)"
