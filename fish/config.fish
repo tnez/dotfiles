7e45d7c9033160a139280bda672ab4b5555f93f7
Travis Nesland <tnesland@gmail.com>
2018-12-11 23:13:08 -0500

    chore: cleanup fish files (#17)
    * Fix lint errors in config file
    
    * Remove path modification
    
    It seems like the correct path is already inherited from the system and if we do need to do any specific modification we should probably set `fish_user_paths` as part of an install script.
    
    * Remove rbenv configuration
    
    From now on, I am going to use docker containers to better reproduce specific environments. I can always add again if I need to, but I think we're fine getting rid of **rbenv**.
    
    * Add README
    
    Include notes for creating and updating local fish packages.

--- fish/config.fish
+++ fish/config.fish
@@ -14,13 +14,12 @@ alias ll="ls -al"
 alias tmux="tmux -2"
 
 # set variables
-export EDITOR="vim"
-export LANG="en_US.UTF-8"
-export LC_ALL="en_US.UTF-8"
-export PAGER="less"
-export PATH="$HOME/bin:/usr/local/bin:/usr/local/sbin:"(brew --prefix qt@5.5)"/bin:$PATH"
-export TERM=xterm-256color
-export VISUAL="vim"
+set -x EDITOR vim
+set -x LANG en_US.UTF-8
+set -x LC_ALL en_US.UTF-8
+set -x PAGER less
+set -x TERM xterm-256color
+set -x VISUAL vim
 
 # install plugin manger (fisher) and any missing plugins
 if not functions -q fisher
@@ -28,9 +27,6 @@ if not functions -q fisher
     curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
     fish -c fisher
 end
-
-# setup rbenv
-status --is-interactive; and source (rbenv init -|psub)
 
 # emacs ansi-term support
 if test -n "$EMACS"
@@ -44,7 +40,7 @@ end
 
 # Base16 Shell
 if status --is-interactive
-    set BASE16_SHELL "$HOME/.config/base16-shell/"
-    source "$BASE16_SHELL/profile_helper.fish"
+    set BASE16_SHELL $HOME/.config/base16-shell/
+    source $BASE16_SHELL/profile_helper.fish
 end
 
