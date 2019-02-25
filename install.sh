21d48f157e83e17989c4afa443f530c735f55714
Travis Nesland <tnesland@gmail.com>
2019-02-25 14:55:57 -0500

    Add install script
    Add an install script to be used after cloning repository. Currently the
    only thing this script takes care of is symlinking files, but installing
    other dependencies could be included as part of the workflow.

---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env sh
+
+CONFIG_DIR="$HOME/Projects/tnez--config"
+
+# symlink from config dir to home dir
+# $1: item to be symlinked (tnez--config/<path>)
+# $2: resulting link ($HOME/<path>)
+_link() {
+  echo "sym-linking $CONFIG_DIR/$1 as $HOME/$2"
+  unlink "$HOME/$2" > /dev/null 2>&1
+  ln -s "$CONFIG_DIR/$1" "$HOME/$2"
+}
+
+_link .editorconfig .editorconfig
+_link .npmrc .npmrc
+_link base16-shell .config/base16-shell
+_link chunkwm/config .chunkwmrc
+_link ctags/config .ctags
+_link fish .config/fish
+_link git/gitconfig .gitconfig
+_link i3 .config/i3
+_link kitty .config/kitty
+_link scripts bin
+_link secrets/.secrets .secrets
+_link skhd/config .skhdrc
+_link spacemacs/.spacemacs .spacemacs
+_link tmux/config .tmux.conf
+_link tnez-snippets .config/tnez-snippets
+_link vim/config .vimrc
