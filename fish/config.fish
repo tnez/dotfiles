5eaa0c00dde73b81a7f9618eca9520e400712fe3
Travis Nesland <tnesland@gmail.com>
2019-03-06 09:57:10 -0500

    Use rbenv to manage ruby versions
    I had previously installed a global ruby version via homebrew. But it is
    probably better to use `rbenv`.

--- fish/config.fish
+++ fish/config.fish
@@ -51,3 +51,6 @@ if status --is-interactive
     set BASE16_SHELL $HOME/.config/base16-shell/
     source $BASE16_SHELL/profile_helper.fish
 end
+
+# Init rbenv
+status --is-interactive; and source (rbenv init - | source)
