b50167f7ff356b233a28c77f5637437efb39efce
Travis Nesland <tnesland@gmail.com>
2020-09-23 13:43:12 -0400

    scripts: tweak my display toggling script

--- scripts/toggle-display.sh
+++ scripts/toggle-display.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
 
-INTERNAL_MONITOR="eDP-1"
-EXTERNAL_MONITOR="DP-1"
+is_connected () {
+  [ $1 -gt 0 ]
+}
 
-if xrandr | grep "$EXTERNAL_MONITOR disconnected"; then
-  xrandr --output "$EXTERNAL_MONITOR" --off \
-    --output "$INTERNAL_MONITOR" --auto --mode 1920x1080
+DP1=$(xrandr | egrep -c '^DP-1 connected')
+DP2=$(xrandr | egrep -c '^DP-2 connected')
+
+# Turn off the internal monitor if we are connected to an external monitor.
+# Note: I never use the laptop monitor when connected to external. If you do you
+# will need to tweak this.
+if is_connected $DP1 || is_connected $DP2; then
+  xrandr --output eDP-1 --off
 else
-  xrandr --output "$INTERNAL_MONITOR" --off \
-    --output "$EXTERNAL_MONITOR" --auto
+  xrandr --output eDP-1 --auto --mode 1920x1080
+fi
+
+# Turn on DP-1 if it is connected
+if is_connected $DP1; then
+  xrandr --output DP-1 --auto
+fi
+
+# Turn on DP-2 if it is connected
+if is_connected $DP2; then
+  xrandr --output DP-2 --auto
 fi
