84ead3d43478d4654b780ac393dc3f96e17bba46
Travis Nesland <tnesland@gmail.com>
2021-01-21 23:25:58 -0500

    Various updates after distro switch

--- xinit/config.sh
+++ xinit/config.sh
@@ -1,12 +1,21 @@
 # disable power save features
-setterm -blank 0 -powersave off
+xset s off
+xsset -dpms
+
+# keyboard layout
+setxkbmap -layout us
+setxkbmap -option ctrl:swapcaps
+setxkbmap -option altwin:swap_alt_win
 
 # faster key repeat and delay
 xset r rate 210 40
 
 # modify my PATH and such
 . $HOME/.profile
-. $HOME/.secrets
+# . $HOME/.secrets
+
+# startup ssh-agent
+eval $(ssh-agent)
 
 # startup the window manager
 exec i3
