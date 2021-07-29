2f9c2f49c8bd465aa0fa5a6a37d41d77f7028b08
Travis Nesland <tnesland@gmail.com>
2021-07-29 14:37:41 -0400

    vim: add octo plugin for better github integration

--- lvim/lv-config.lua
+++ lvim/lv-config.lua
@@ -116,6 +116,9 @@ O.user_plugins = {
         "ray-x/lsp_signature.nvim",
         config = function() require"lsp_signature".on_attach() end,
         event = "InsertEnter"
+    }, {
+        "pwntester/octo.nvim",
+        event = "BufRead"
     }
 }
 
