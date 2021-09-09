81341e7ba3a9ba8d24b5b0ad7b615be941bf5b88
Travis Nesland <tnesland@gmail.com>
2021-09-09 10:35:55 -0400

    vim: cleanup config file

--- lvim/lv-config.lua
+++ lvim/lv-config.lua
@@ -1,73 +1,15 @@
--- general
+----------
+-- GENERAL
+--
 lvim.format_on_save = true
 lvim.lint_on_save = true
--- Uncoment for light theme
--- vim.g.tokyonight_style = "day"
-lvim.colorscheme = "tokyonight"
+lvim.colorscheme = "xresources"
 
--- keymappings
-lvim.leader = "space"
--- overwrite the key-mappings provided by LunarVim for any mode, or leave it empty to keep them
--- lvim.keys.normal_mode = {
---   Page down/up
---   {'[d', '<PageUp>'},
---   {']d', '<PageDown>'},
+--------------
+-- KEYMAPPINGS
 --
---   Navigate buffers
---   {'<Tab>', ':bnext<CR>'},
---   {'<S-Tab>', ':bprevious<CR>'},
--- }
--- if you just want to augment the existing ones then use the utility function
--- require("utils").add_keymap_insert_mode({ silent = true }, {
--- { "<C-s>", ":w<cr>" },
--- { "<C-c>", "<ESC>" },
--- })
--- you can also use the native vim way directly
--- vim.api.nvim_set_keymap("i", "<C-Space>", "compe#complete()", { noremap = true, silent = true, expr = true })
-
--- TODO: User Config for predefined plugins
--- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
-lvim.builtin.lualine.active = true
-lvim.builtin.dashboard.active = true
-lvim.builtin.terminal.active = true
-
--- if you don't want all the parsers change this to a table of the ones you want
-lvim.builtin.treesitter.ensure_installed = {}
-lvim.builtin.treesitter.ignore_install = { "haskell" }
-lvim.builtin.treesitter.highlight.enabled = true
-
--- generic LSP settings
--- you can set a custom on_attach function that will be used for all the language servers
--- See <https://github.com/neovim/nvim-lspconfig#keybindings-and-completion>
--- lvim.lsp.on_attach_callback = function(client, bufnr)
---   local function buf_set_option(...)
---     vim.api.nvim_buf_set_option(bufnr, ...)
---   end
---   --Enable completion triggered by <c-x><c-o>
---   buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
--- end
-
--- Additional Plugins
-lvim.plugins = {
-  {"folke/tokyonight.nvim"},
-  {
-    "folke/trouble.nvim",
-    cmd = "TroubleToggle",
-  },
-  {
-    "ray-x/lsp_signature.nvim",
-    config = function() require"lsp_signature".on_attach() end,
-    event = "InsertEnter"
-  },
-  {"sotte/presenting.vim"},
-}
-
--- Autocommands (https://neovim.io/doc/user/autocmd.html)
--- lvim.autocommands.custom_groups = {
---   { "BufWinEnter", "*.lua", "setlocal ts=8 sw=8" },
--- }
-
--- Additional Leader bindings for WhichKey
+lvim.leader = "space"
+-- WhichKey
 lvim.builtin.which_key.mappings["t"] = {
   name = "Diagnostics",
   t = { "<cmd>TroubleToggle<cr>", "trouble" },
@@ -77,7 +19,50 @@ lvim.builtin.which_key.mappings["t"] = {
   l = { "<cmd>TroubleToggle loclist<cr>", "loclist" },
   r = { "<cmd>TroubleToggle lsp_references<cr>", "references" },
 }
-
 lvim.builtin.which_key.mappings["P"] = {
   "<cmd>PresentingStart<cr>", "Present"
+}
+
+------------------
+-- BUILTIN PLUGINS
+--
+lvim.builtin.lualine.active = true
+lvim.builtin.dashboard.active = true
+lvim.builtin.terminal.active = true
+
+-------------------
+-- TREESITTER STUFF
+--
+lvim.builtin.treesitter.ensure_installed = "maintained"
+lvim.builtin.treesitter.ignore_install = { "haskell" }
+lvim.lang.javascript.formatters = {
+  {
+    exe = 'prettier',
+    args = {},
+  }
+}
+lvim.lang.javascript.linters = {
+  {
+    exe = 'xo',
+    args = {},
+  }
+}
+
+---------------------
+-- ADDITIONAL PLUGINS
+--
+lvim.plugins = {
+  {"folke/tokyonight.nvim"},
+  {
+    "folke/trouble.nvim",
+    cmd = "TroubleToggle",
+  },
+  {"nekonako/xresources-nvim"},
+  {
+    "ray-x/lsp_signature.nvim",
+    config = function() require"lsp_signature".on_attach() end,
+    event = "InsertEnter"
+  },
+  {"sotte/presenting.vim"},
+  {"xojs/vim-xo"},
 }
