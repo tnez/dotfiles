565ffcb07bc6f15e5c7d9745e1cee91e9cb8023d
Travis Nesland <tnesland@gmail.com>
2021-12-22 16:51:32 -0500

    Tweak whichkey bindings for terminal toggle

--- nvim/lua/user/whichkey.lua
+++ nvim/lua/user/whichkey.lua
@@ -173,13 +173,12 @@ local mappings = {
 
   t = {
     name = "Terminal",
+    ["-"] = { "<cmd>ToggleTerm size=10 direction=horizontal<cr>", "Horizontal" },
+    ["/"] = { "<cmd>ToggleTerm size=80 direction=vertical<cr>", "Vertical" },
+    t = { "<cmd>ToggleTerm direction=float<cr>", "Float" },
     n = { "<cmd>lua _NODE_TOGGLE()<cr>", "Node" },
-    u = { "<cmd>lua _NCDU_TOGGLE()<cr>", "NCDU" },
-    t = { "<cmd>lua _HTOP_TOGGLE()<cr>", "Htop" },
     p = { "<cmd>lua _PYTHON_TOGGLE()<cr>", "Python" },
-    f = { "<cmd>ToggleTerm direction=float<cr>", "Float" },
-    h = { "<cmd>ToggleTerm size=10 direction=horizontal<cr>", "Horizontal" },
-    v = { "<cmd>ToggleTerm size=80 direction=vertical<cr>", "Vertical" },
+    u = { "<cmd>lua _HTOP_TOGGLE()<cr>", "Usage - Htop" },
   },
 }
 
