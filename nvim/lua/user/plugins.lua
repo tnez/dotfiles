7c6b021b41201e82bf14b7a02b4bd173baa727b3
Travis Nesland <tnesland@gmail.com>
2021-12-23 10:55:37 -0500

    vim: better tmux / vim navigation

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -58,6 +58,7 @@ return packer.startup(function(use)
   use "goolord/alpha-nvim"
   use "antoinemadec/FixCursorHold.nvim" -- This is needed to fix lsp doc highlight
   use "folke/which-key.nvim"
+  use "christoomey/vim-tmux-navigator"
 
   -- Colorschemes
   use 'folke/tokyonight.nvim'
