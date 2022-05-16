776b53ed0727694a4b764078096491bc738d3901
Travis Nesland <tnesland@gmail.com>
2022-05-16 12:21:42 -0400

    Updates for Neovim 0.8

--- nvim/lua/user/lsp/null-ls.lua
+++ nvim/lua/user/lsp/null-ls.lua
@@ -11,6 +11,7 @@ local diagnostics = null_ls.builtins.diagnostics
 null_ls.setup({
 	debug = false,
 	sources = {
+		diagnostics.eslint,
 		formatting.prettier,
 		formatting.black.with({ extra_args = { "--fast" } }),
 		formatting.stylua,
