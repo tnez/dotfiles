aee4d6b534063b9486efd2f696f412e46a3648f0
Travis Nesland <tnesland@gmail.com>
2022-07-13 11:16:56 -0400

    Rework `dotfiles` for GitHub Codespaces

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+# Make a vendor directory
+echo "[info] making vendor directory"
+mkdir ${HOME}/vendor
+
+# Install LazyGit
+# echo "[info] installing lazygit"
+# cd ${HOME}/vendor
+# git clone https://github.com/jesseduffield/lazygit.git
+# cd lazygit
+# go install
+# cd ${HOME}
+
+# Aliases
+alias cls='clear'
+alias g='git'
+
+# Complete!
+echo "[info] succcess!"
