ee49ff4ce14397194dcc9669ff15eaafa7826664
Travis Nesland <tnesland@gmail.com>
2022-09-22 20:52:05 +0000

    fix: update proc to install `lazygit`

--- install.sh
+++ install.sh
@@ -1,16 +1,11 @@
 #!/usr/bin/env sh
 
-# Make a vendor directory
-echo "[info] making vendor directory"
-mkdir ${HOME}/vendor
-
 # Install LazyGit
 echo "[info] installing lazygit"
-cd ${HOME}/vendor
-git clone https://github.com/jesseduffield/lazygit.git
-cd lazygit
-go install
-cd ${HOME}
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[0-35.]+')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+sudo tar xf lazygit.tar.gz -C /usr/local/bin lazygit
+rm lazygit.tar.gz
 
 # Complete!
 echo "[info] succcess!"
