ef7d310477fbf404f31d1250110eaf84e13aace1
Travis Nesland <tnesland@gmail.com>
2022-11-03 18:26:15 +0000

    Additional migration for work with Github Codespaces

--- fish/tnez/functions/fish_user_key_bindings.fish
+++
@@ -1,9 +0,0 @@
-function fish_user_key_bindings
-    fish_vi_key_bindings
-
-    bind -M insert -m default kj backward-char force-repaint
-
-    for mode in insert default visual
-        bind -M $mode \cf forward-char
-    end
-end
