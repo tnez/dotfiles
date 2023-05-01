07110d2e6bb5a395fa651afd29952b07f318e040
Travis Nesland <tnesland@gmail.com>
2023-05-01 10:40:48 -0400

    Add my usual easy escape key-mappings

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,18 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local function map(mode, lhs, rhs, opts)
+  local keys = require("lazy.core.handler").handlers.keys
+  ---@cast keys LazyKeysHandler
+  -- do not create the keymap if a lazy keys handler exists
+  if not keys.active[keys.parse({ lhs, mode = mode }).id] then
+    opts = opts or {}
+    opts.silent = opts.silent ~= false
+    vim.keymap.set(mode, lhs, rhs, opts)
+  end
+end
+
+map("i", "kj", "<esc>", { desc = "Escape to normal mode" })
+map("i", "jj", "<esc>", { desc = "Escape to normal mode" })
+map("i", "kk", "<esc>", { desc = "Escape to normal mode" })
