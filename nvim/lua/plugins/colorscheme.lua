c08a2b415542d8c0ac1bbc686a7b38b380e2d30a
Travis Nesland <tnesland@gmail.com>
2023-05-11 10:10:38 -0400

    (term) rework kitty config

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,11 @@
+return {
+  { "catppuccin/nvim", lazy = true, name = "catppuccin", opts = {
+    flavour = "frappe",
+  } },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+}
