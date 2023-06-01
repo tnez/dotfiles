7ab29fdd3616e0073659d2e5908fd7f7cb8e4066
Travis Nesland <tnesland@gmail.com>
2023-06-01 14:35:46 -0400

    (vim) configure copilot settings

---
+++ nvim/lua/plugins/extras.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    build = ":Copilot auth",
+    opts = {
+      suggestion = { enabled = true },
+      panel = { enabled = true },
+    },
+  },
+}
