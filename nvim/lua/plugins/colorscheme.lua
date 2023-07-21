eb407fe6914c36d7c06eacfc7a792ccc4525d49c
Travis Nesland <tnesland@gmail.com>
2023-07-21 10:40:42 -0700

    Condition neovim colorscheme on a MODE system env
    If `MODE == 'DARK'` then neovim will use the dark variant. Otherwise it will use
    a light variant.

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,6 +1,13 @@
+-- Get the value of the MODE environment variable and set the flavour accordingly
+local mode = vim.env.MODE
+local inferredFlavour = "latte"
+if mode == "DARK" then
+  inferredFlavour = "frappe"
+end
+
 return {
   { "catppuccin/nvim", lazy = true, name = "catppuccin", opts = {
-    flavour = "frappe",
+    flavour = inferredFlavour,
   } },
   {
     "LazyVim/LazyVim",
