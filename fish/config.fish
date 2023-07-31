97524319b2872b88272a775b839453aeae48837c
Travis Nesland <tnesland@gmail.com>
2023-07-31 10:30:32 -0700

    Add pnpm path to fish config

--- fish/config.fish
+++ fish/config.fish
@@ -45,5 +45,12 @@ function fish_title
     true
 end
 
-test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
+test -e {$HOME}/.iterm2_shell_integration.fish; and source {$HOME}/.iterm2_shell_integration.fish
 
+
+# pnpm
+set -gx PNPM_HOME /Users/tnez/Library/pnpm
+if not string match -q -- $PNPM_HOME $PATH
+    set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
