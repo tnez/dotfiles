0de14ae81c7014456179b6fa20fa559aa36c37da
Travis Nesland <travis@tnez.dev>
2024-01-26 15:49:34 -0500

    Additional mappings for closing buffers

--- astronvim/mappings.lua
+++ astronvim/mappings.lua
@@ -7,5 +7,17 @@ return {
     ["<leader>c"] = false,
     ["<C-c><C-c>"] = { ":Neotest run<cr>", desc = "Testing: Run nearest" },
     ["<C-c><C-o>"] = { ":Neotest summary<cr>", desc = "Testing: Open summary" },
+    ["<leader>bd"] = {
+      function()
+        require("astronvim.utils.buffer").close()
+      end,
+      desc = "Close buffer",
+    },
+    ["<leader>bo"] = {
+      function()
+        require("astronvim.utils.buffer").close_all(true)
+      end,
+      desc = "Close all buffers except for current",
+    },
   },
 }
