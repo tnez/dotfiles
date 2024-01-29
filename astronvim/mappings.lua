ce07448c27e370e7c04c56d8f7fde03d9349962d
Travis Nesland <travis@tnez.dev>
2024-01-29 17:09:12 -0500

    Add keybindings for navigating between buffers
    - Mapped `<S-H>` to `:bp` (previous buffer)
    - Mapped `<S-L>` to `:bn` (next buffer)

--- astronvim/mappings.lua
+++ astronvim/mappings.lua
@@ -7,6 +7,8 @@ return {
     ["<leader>c"] = false,
     ["<C-c><C-c>"] = { ":Neotest run<cr>", desc = "Testing: Run nearest" },
     ["<C-c><C-o>"] = { ":Neotest summary<cr>", desc = "Testing: Open summary" },
+    ["<S-H>"] = { ":bp<CR>", desc = "Jump to previous buffer" },
+    ["<S-L>"] = { ":bn<CR>", desc = "Jump to next buffer" },
     ["<leader>bd"] = {
       function()
         require("astronvim.utils.buffer").close()
