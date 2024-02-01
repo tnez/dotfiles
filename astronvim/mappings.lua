f46c93ac4d42593fb5d96f54ba8bf0d19c15288d
Travis Nesland <travis@tnez.dev>
2024-02-01 12:16:08 -0500

    Add mapping to run neotest with DAP

--- astronvim/mappings.lua
+++ astronvim/mappings.lua
@@ -5,6 +5,12 @@ return {
   n = {
     ["kj"] = false,
     ["<C-c><C-c>"] = { ":Neotest run<cr>", desc = "Testing: Run nearest" },
+    ["<C-c><C-d>"] = {
+      function()
+        require("neotest").run.run({ strategy = "dap" })
+      end,
+      desc = "Testing: Run nearest with debugger",
+    },
     ["<C-c><C-o>"] = { ":Neotest summary<cr>", desc = "Testing: Open summary" },
     ["<S-H>"] = { ":bp<CR>", desc = "Jump to previous buffer" },
     ["<S-L>"] = { ":bn<CR>", desc = "Jump to next buffer" },
