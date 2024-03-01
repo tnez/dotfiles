1416969227c064ac2f722f053d9c03b3bac43547
Travis Nesland <travis@tnez.dev>
2024-03-01 10:46:32 -0500

    editor: add keybinding to diff current PR

--- .config/nvim/lua/plugins/github.lua
+++ .config/nvim/lua/plugins/github.lua
@@ -1,5 +1,6 @@
 return {
   "pwntester/octo.nvim",
+  lazy = false,
   depends = {
     "nvim-lua/plenary.nvim",
     "nvim-telescope/telescope.nvim",
@@ -14,6 +15,7 @@ return {
   keys = {
     { "<leader>go", desc = "Octo -> " },
     { "<leader>goc", ":Octo pr create<CR>", desc = "Create New Pull-Request" },
+    { "<leader>god", ":Octo pr diff<CR>", desc = "Diff current Pull-Request" },
     { "<leader>gol", ":Octo pr list<CR>", desc = "List Open Pull-Requests" },
     { "<leader>goo", ":Octo pr open<CR>", desc = "Open current Pull-Request" },
   },
