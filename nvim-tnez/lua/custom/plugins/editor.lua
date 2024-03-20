6c1238cf3fbba842edd516aaf42b606657dd24e9
Travis Nesland <travis@tnez.dev>
2024-03-20 10:04:19 -0400

    (editor) add harpoon for jumping between marks

--- nvim-tnez/lua/custom/plugins/editor.lua
+++ nvim-tnez/lua/custom/plugins/editor.lua
@@ -140,4 +140,46 @@ return {
       --  Check out: https://github.com/echasnovski/mini.nvim
     end,
   },
+
+  { -- For makring and jumping around buffers
+    'ThePrimeagen/harpoon',
+    branch = 'harpoon2',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      local harpoon = require 'harpoon'
+      harpoon:setup()
+
+      -- Setting Marks
+      vim.keymap.set('n', '<C-m>', function()
+        harpoon:list():append()
+      end, { desc = 'Append to Harpoon Marks' })
+
+      -- Searching Marks
+      local conf = require('telescope.config').values
+      local function toggle_telescope(harpoon_files)
+        local file_paths = {}
+        for _, item in ipairs(harpoon_files.items) do
+          table.insert(file_paths, item.value)
+        end
+
+        require('telescope.pickers')
+          .new({}, {
+            prompt_title = 'Harpoon',
+            finder = require('telescope.finders').new_table {
+              results = file_paths,
+            },
+            previewer = conf.file_previewer {},
+            sorter = conf.generic_sorter {},
+          })
+          :find()
+      end
+
+      vim.keymap.set('n', '<leader>sm', function()
+        toggle_telescope(harpoon:list())
+      end, { desc = '[S]earch Harpoon [M]arks' })
+      vim.keymap.set('n', '<leader><leader>', function()
+        toggle_telescope(harpoon:list())
+      end, { desc = '[S]earch Harpoon [M]arks' })
+    end,
+  },
 }
