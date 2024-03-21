e9bba57cfab01f433779733af06ab2f3ea9a187c
Travis Nesland <travis@tnez.dev>
2024-03-21 16:12:39 -0400

    (editor): don't fold so much on open

--- nvim-tnez/lua/options.lua
+++ nvim-tnez/lua/options.lua
@@ -64,4 +64,4 @@ vim.opt.scrolloff = 10
 vim.opt.foldmethod = 'expr'
 vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
 vim.opt.foldenable = true
-vim.opt.foldlevelstart = 2
+vim.opt.foldlevelstart = 99
