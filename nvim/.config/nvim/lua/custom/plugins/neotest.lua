2764623577425cb9fdb345d6d5e3db0f7dd7d265
Travis Nesland <travis@tnez.dev>
2024-09-27 11:58:48 -0400

    Fix which-key setup

--- nvim/.config/nvim/lua/custom/plugins/neotest.lua
+++ nvim/.config/nvim/lua/custom/plugins/neotest.lua
@@ -69,7 +69,6 @@ return {
 
   -- stylua: ignore
   keys = {
-    {"<leader>t", "", desc = "[T]est"},
     { "<leader>tt", function() require("neotest").run.run(vim.fn.expand("%")) end, desc = "Run File" },
     { "<leader>tT", function() require("neotest").run.run(vim.uv.cwd()) end, desc = "Run All Test Files" },
     { "<leader>tr", function() require("neotest").run.run() end, desc = "Run Nearest" },
