dfbeead842e50498886fe9331a5171494662ce6b
Travis Nesland <travis@tnez.dev>
2024-09-27 11:58:48 -0400

    Fix which-key setup

--- nvim/.config/nvim/lua/custom/plugins/which-key.lua
+++ nvim/.config/nvim/lua/custom/plugins/which-key.lua
@@ -1,22 +1,19 @@
 return { -- Useful plugin to show you pending keybinds.
   'folke/which-key.nvim',
-  event = 'VimEnter', -- Sets the loading event to 'VimEnter'
-  config = function() -- This is the function that runs, AFTER loading
-    require('which-key').setup()
-
-    -- Document existing key chains
-    -- TODO: We should defer these to relevant plugins
-    require('which-key').register {
-      ['<leader>a'] = { name = '[A]I', _ = 'which_key_ignore' },
-      ['<leader>b'] = { name = '[B]uffer', _ = 'which_key_ignore' },
-      ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-      ['<leader>d'] = { name = '[D]ebug', _ = 'which_key_ignore' },
-      ['<leader>o'] = { name = '[O]pen', _ = 'which_key_ignore' },
-      ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-      ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-      ['<leader>t'] = { name = '[T]est', _ = 'which_key_ignore' },
-      ['<leader>u'] = { name = '[U]I', _ = 'which_key_ignore' },
-      ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
+  event = 'VeryLazy',
+  config = function()
+    local wk = require 'which-key'
+    wk.add {
+      { '<leader>a', group = 'ai' },
+      { '<leader>b', group = 'buffer' },
+      { '<leader>c', group = 'code' },
+      { '<leader>d', group = 'debug' },
+      { '<leader>o', group = 'open' },
+      { '<leader>r', group = 'rename' },
+      { '<leader>s', group = 'search' },
+      { '<leader>t', group = 'test' },
+      { '<leader>u', group = 'ui' },
+      { '<leader>w', group = 'workspace' },
     }
   end,
 }
