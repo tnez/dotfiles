40b8ca70c91e41b47d9bf3e75e070192bd367262
Travis Nesland <travis@tnez.dev>
2025-01-02 22:52:59 -0500

    (editor) some initial snippet setup with `blink.cmp`

--- nvim/.config/nvim/lua/custom/plugins/autocompletion.lua
+++ nvim/.config/nvim/lua/custom/plugins/autocompletion.lua
@@ -1,6 +1,7 @@
 return {
   'saghen/blink.cmp',
-  -- optional: provides snippets for the snippet source
+  version = 'v0.*',
+  build = 'cargo build --release',
   dependencies = {
     {
       'giuxtaposition/blink-cmp-copilot',
@@ -19,8 +20,6 @@ return {
     'rafamadriz/friendly-snippets',
   },
 
-  -- use a release tag to download pre-built binaries
-  version = 'v0.*',
   -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
   -- build = 'cargo build --release',
   -- If you use nix, you can build from source using latest nightly rust with:
@@ -58,8 +57,7 @@ return {
         },
       },
       default = { 'lsp', 'path', 'snippets', 'copilot', 'buffer' },
-      -- optionally disable cmdline completions
-      -- cmdline = {},
+      cmdline = {},
     },
 
     -- experimental signature help support
