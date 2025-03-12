f11be1f03bfad115e76ce82ab44bde24248a8945
Travis Nesland <travis@tnez.dev>
2025-03-12 09:07:26 -0400

    Update my neovim python hack to work with uv

--- fish/.config/fish/tnez/functions/nvim.fish
+++ fish/.config/fish/tnez/functions/nvim.fish
@@ -1,7 +1,16 @@
 function nvim
   if test -d .venv
-     echo "Found .venv directory, running 'poetry run nvim'"
-     poetry run /usr/bin/env nvim $argv
+    if test -f poetry.lock
+      echo "Found .venv directory and poetry.lock, running 'poetry run nvim'"
+      poetry run /usr/bin/env nvim $argv
+    else if test -f uv.lock
+      echo "Found .venv directory and uv.lock, running 'uv run nvim'"
+      uv run /usr/bin/env nvim $argv
+    else
+      echo "Found .venv directory but no lock file, running 'nvim'"
+      /usr/bin/env nvim $argv
+    end
+
   else
      echo "No .venv directory found, running 'nvim'"
      /usr/bin/env nvim $argv
